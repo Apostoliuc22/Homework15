55dfad86bb41639361570352a19c157570e85d29
Apostoliuc22 <apostoliuc.alex@gmail.com>
2020-02-02 20:22:29 +0200

    eslint

---
+++ java.js
@@ -0,0 +1,20 @@
+
+function Descr(a, b, c) {
+    var D;
+    var x1;
+    var x2;
+    D = (b * b) - 4 * a * c;
+    if (D < 0) {
+        console.log("Нет действительных корней");
+    }
+    else if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        console.log("x1 = " + x1 + " x2= " + x2);
+    }
+    else if (D == 0) {
+        x = ((-b) / (2 * a));
+    }
+}
+Descr(1, -70, 600);
+
